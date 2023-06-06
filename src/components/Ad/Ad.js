b73021b34955350fb4f702339439d55f105699ed
lizaK2006 <kirsanovaliza2019@gmail.com>
2023-06-06 16:59:31 +0300

    hw

---
+++ src/components/Ad/Ad.js
@@ -0,0 +1,18 @@
+import styles from "./Ad.module.css"
+export default function Ad(){
+    items: [
+        {   
+             id: 1,
+            title: "water",
+            description: "water",
+            price: "1.5 " 
+        },
+        {   
+            id: 2,
+           title: "phone",
+           description: "samsung",
+           price: "1000"  
+       }
+    ]
+    return(items)
+}
